0d4da4bede8e9e8883bd16dd64537f8ec89c68bb
jreps <jreps@its.jnj.com>
2020-03-29 15:39:33 -0400

    Update main.R
    fixed oracleTempSchema missing in createCohorts bug

--- R/main.R
+++ R/main.R
@@ -56,6 +56,7 @@ execute <- function(connectionDetails,
                   cdmDatabaseSchema=cdmDatabaseSchema,
                   cohortDatabaseSchema=cohortDatabaseSchema,
                   cohortTable=cohortTable,
+                  oracleTempSchema = oracleTempSchema,
                   outputFolder = outputFolder)
   }
 
